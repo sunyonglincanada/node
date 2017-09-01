6382feb3d2ec7a5e49090d44a05290f89a770edb
SUN YONGLIN <sunyonglincanada@gmail.com>
2017-09-01 00:12:43 -0400

    created and imported modules and echo module content

---
+++ app.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var module1 = require('./module');
+var module2 = require('./module2');
+
+function onRequest(request, response){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write(module1.myString);
+    response.write(module2.myVariable);
+    module1.myFunction();
+    module2.myFunction();
+    response.end();
+}
+
+http.createServer(onRequest).listen(3000);
---
+++ module.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    console.log('Function was Called');
+}
+var myString = 'string';
+
+module.exports.myFunction = myFunction;
+module.exports.myString = myString;
