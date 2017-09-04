5d851eb7b50621d9dc0da6cb7289a1fd06ee02fa
SUN YONGLIN <sunyonglincanada@gmail.com>
2017-09-04 15:59:50 -0400

    feat: setup express framework in node js

--- app.js
+++ app.js
@@ -1,79 +1,46 @@
-var module1 = require('./module');
-var module2 = require('./module2');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var fs = require('fs');
-var url = require('url');
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-/**
- * Rendering HTML with Routes
- * @param path
- * @param response
- * @date 2017-09-04
- */
-function renderHTML(path,response){
-    fs.readFile(path, null, function(error, data){
-        if(error){
-            response.writeHead(404);
-            response.write('File not found');
-        }else{
-            response.write(data);
-        }
-        response.end();
-    });
-}
+var app = express();
 
-/**
- * Define Request Routes
- * @type {{handleRequest: module.exports.handleRequest}}
- */
-module.exports = {
-    handleRequest: function(request,response){
-        response.writeHead(200, {'Content-Type': 'text/html'});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-        var path = url.parse(request.url).pathname;
-        switch (path){
-            case '/':
-                renderHTML('./index.html',response);
-                break;
-            case '/login':
-                renderHTML('./login.html', response);
-                break;
-            default:
-                response.writeHead(404);
-                response.write('Route not defined')
-                response.end();
-        }
-    }
-};
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', index);
+app.use('/users', users);
 
-function onRequest(request, response){
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write(module1.myString);
-    response.write(module2.myVariable);
-    module1.myFunction();
-    module2.myFunction();
-    response.end();
-}
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-/**
- * Show HTML Content
- * @param request
- * @param response
- *
- * @date 2017-09-04
- */
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-function onRequestIndex(request, response){
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./index.html',null,function(error, data){
-        if(error){
-            response.writeHead(404);
-            response.write('File Not Found!')
-        }else{
-            response.write(data);
-        }
-        response.end();
-    });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-}
+module.exports = app;
